e6d40ecb35614c3ba58221e304ccb66bd383a843
Wildan Ziaulhaq <wildan2wildan@gmail.com>
2018-10-29 20:30:40 +0700

    Adding search_bar component

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 
+import SearchBar from './search_bar';
+
+const API_KEY = 'AIzaSyCrGuymQRs9fW9bFr8p4ZPldPZTYO0CCm8';
+
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <SearchBar/>
+      </div>
     );
   }
 }
