c3eb1ce2afc99ec84759ef2fdbcbadc77de757a4
Wildan Ziaulhaq <wildan2wildan@gmail.com>
2018-11-04 20:38:12 +0700

    Make a list of videos

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './search_bar';
+import VideoList from './video_list';
 
 const API_KEY = 'AIzaSyCrGuymQRs9fW9bFr8p4ZPldPZTYO0CCm8';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'reactjs' }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
   render() {
     return (
       <div>
         <SearchBar/>
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
