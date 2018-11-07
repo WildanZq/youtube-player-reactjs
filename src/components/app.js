f2b7f27158919d9c09e43e3595250a064be091cf
Wildan Ziaulhaq <wildan2wildan@gmail.com>
2018-11-07 16:10:09 +0700

    Add video player

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './search_bar';
 import VideoList from './video_list';
+import VideoDetail from './video_detail';
 
 const API_KEY = 'AIzaSyCrGuymQRs9fW9bFr8p4ZPldPZTYO0CCm8';
 
@@ -20,6 +21,7 @@ export default class App extends Component {
     return (
       <div>
         <SearchBar/>
+        <VideoDetail video={this.state.videos[0]}/>
         <VideoList videos={this.state.videos} />
       </div>
     );
